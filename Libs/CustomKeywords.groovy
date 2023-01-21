3ddb71d64d33ed0026eca4c9ff96736352130033
rahulrathore44 <rahulrathore44@LRRS>
2023-01-21 12:01:08 -0800

    Added the custom keyword

--- Libs/CustomKeywords.groovy
+++ Libs/CustomKeywords.groovy
@@ -9,16 +9,74 @@ import com.kms.katalon.core.testobject.TestObject
 
 
 
-def static "com.helper.sftp.SftpKeyword.sftpDownloadUpload"(
-    	String localFile	
-     , 	String remoteDir	
-     , 	String remoteFile	
-     , 	String updateContent	) {
-    (new com.helper.sftp.SftpKeyword()).sftpDownloadUpload(
-        	localFile
-         , 	remoteDir
-         , 	remoteFile
-         , 	updateContent)
+def static "com.helper.navigate.NavigationHelper.navigateToMenuItem"(
+    	String menuName	) {
+    (new com.helper.navigate.NavigationHelper()).navigateToMenuItem(
+        	menuName)
+}
+
+
+def static "com.question.UniqueScreenShot.takeScreenShot"(
+    	String path	
+     , 	String name	) {
+    (new com.question.UniqueScreenShot()).takeScreenShot(
+        	path
+         , 	name)
+}
+
+
+def static "com.question.CustomJsonParser.jsonExtractor"(
+    	String response	) {
+    (new com.question.CustomJsonParser()).jsonExtractor(
+        	response)
+}
+
+
+def static "com.question.mongo.connectRobo3t.connection"(
+    	String url	) {
+    (new com.question.mongo.connectRobo3t()).connection(
+        	url)
+}
+
+
+def static "com.helper.login.LoginHelper.loginIntoApplication"(
+    	String applicationUrl	
+     , 	String Username	
+     , 	String Password	) {
+    (new com.helper.login.LoginHelper()).loginIntoApplication(
+        	applicationUrl
+         , 	Username
+         , 	Password)
+}
+
+
+def static "com.helper.login.LoginHelper.loginIntoApplicationWith"() {
+    (new com.helper.login.LoginHelper()).loginIntoApplicationWith()
+}
+
+
+def static "com.helper.login.LoginHelper.logoutFromApplication"() {
+    (new com.helper.login.LoginHelper()).logoutFromApplication()
+}
+
+
+def static "com.question.CustomSendKeys.TypeInText"(
+    	TestObject aLocator	
+     , 	String text	) {
+    (new com.question.CustomSendKeys()).TypeInText(
+        	aLocator
+         , 	text)
+}
+
+
+def static "com.question.CustomSendKeys.TypeInTextBoxWithWait"(
+    	TestObject aLocator	
+     , 	String text	
+     , 	int timeOutInSeconds	) {
+    (new com.question.CustomSendKeys()).TypeInTextBoxWithWait(
+        	aLocator
+         , 	text
+         , 	timeOutInSeconds)
 }
 
 
@@ -37,35 +95,46 @@ def static "com.helper.excel.ExcelHelper.writeTOExcelFile"(
 }
 
 
-def static "com.helper.javascript.JavaScriptHelper.executeScript"(
-    	String script	) {
-    (new com.helper.javascript.JavaScriptHelper()).executeScript(
-        	script)
+def static "com.question.CustomDatabase.executeQuery"(
+    	String connectionUrl	
+     , 	String query	) {
+    (new com.question.CustomDatabase()).executeQuery(
+        	connectionUrl
+         , 	query)
 }
 
 
-def static "com.helper.javascript.JavaScriptHelper.executeScriptOnTestObject"(
-    	String string	
-     , 	TestObject testObject	) {
-    (new com.helper.javascript.JavaScriptHelper()).executeScriptOnTestObject(
-        	string
-         , 	testObject)
+def static "com.question.VerifyMultipleElement.verifyElement"(
+    	String[] testObject	) {
+    (new com.question.VerifyMultipleElement()).verifyElement(
+        	testObject)
 }
 
 
-def static "com.helper.javascript.JavaScriptHelper.executeAndReturnValue"(
-    	String script	
-     , 	TestObject testObject	) {
-    (new com.helper.javascript.JavaScriptHelper()).executeAndReturnValue(
-        	script
-         , 	testObject)
+def static "com.helper.sftp.SftpKeyword.sftpDownloadUpload"(
+    	String localFile	
+     , 	String remoteDir	
+     , 	String remoteFile	
+     , 	String updateContent	) {
+    (new com.helper.sftp.SftpKeyword()).sftpDownloadUpload(
+        	localFile
+         , 	remoteDir
+         , 	remoteFile
+         , 	updateContent)
 }
 
-/**
+
+def static "com.question.GetTestDataFilePath.getAbsolutePath"(
+    	String FileNameWithFolder	) {
+    (new com.question.GetTestDataFilePath()).getAbsolutePath(
+        	FileNameWithFolder)
+}
+
+ /**
 	 * @param topDropDown :- Locator of the top drop down which have item count
 	 * @param dropDownItemCount :- Count of the item present in the topDropDown
 	 * @param gridItemTestObject :- Base locator for the item present in the grid
-	 */
+	 */ 
 def static "com.question.VerifyInItemGrid.verifyItemInTheGrid"(
     	TestObject topDropDown	
      , 	int dropDownItemCount	
@@ -95,31 +164,40 @@ def static "com.question.UniqueSelection.SelectCheckBox"() {
 }
 
 
-def static "com.question.CustomJsonParser.jsonExtractor"(
-    	String response	) {
-    (new com.question.CustomJsonParser()).jsonExtractor(
-        	response)
+def static "com.helper.javascript.JavaScriptHelper.executeScript"(
+    	String script	) {
+    (new com.helper.javascript.JavaScriptHelper()).executeScript(
+        	script)
+}
+
+
+def static "com.helper.javascript.JavaScriptHelper.executeScriptOnTestObject"(
+    	String string	
+     , 	TestObject testObject	) {
+    (new com.helper.javascript.JavaScriptHelper()).executeScriptOnTestObject(
+        	string
+         , 	testObject)
+}
+
+
+def static "com.helper.javascript.JavaScriptHelper.executeAndReturnValue"(
+    	String script	
+     , 	TestObject testObject	) {
+    (new com.helper.javascript.JavaScriptHelper()).executeAndReturnValue(
+        	script
+         , 	testObject)
+}
+
+
+def static "com.question.ClickAnyLink.wrapperClick"(
+    	String linkName	) {
+    (new com.question.ClickAnyLink()).wrapperClick(
+        	linkName)
 }
 
 
 def static "com.question.GetMetrics.getAlarmsCountfromGA"() {
     (new com.question.GetMetrics()).getAlarmsCountfromGA()
-}
-
-
-def static "com.question.RandomEmail.getEmail"(
-    	String suffix	
-     , 	String prefix	) {
-    (new com.question.RandomEmail()).getEmail(
-        	suffix
-         , 	prefix)
-}
-
-
-def static "com.helper.navigate.NavigationHelper.navigateToMenuItem"(
-    	String menuName	) {
-    (new com.helper.navigate.NavigationHelper()).navigateToMenuItem(
-        	menuName)
 }
 
 
@@ -147,62 +225,24 @@ def static "com.helper.grid.GridHelper.getValueOrClickOnColumn"(
 }
 
 
-def static "com.question.UniqueScreenShot.takeScreenShot"(
-    	String path	
-     , 	String name	) {
-    (new com.question.UniqueScreenShot()).takeScreenShot(
-        	path
-         , 	name)
+def static "com.question.RandomEmail.getEmail"(
+    	String suffix	
+     , 	String prefix	) {
+    (new com.question.RandomEmail()).getEmail(
+        	suffix
+         , 	prefix)
 }
 
 
-def static "com.helper.login.LoginHelper.loginIntoApplication"(
-    	String applicationUrl	
-     , 	String Username	
-     , 	String Password	) {
-    (new com.helper.login.LoginHelper()).loginIntoApplication(
-        	applicationUrl
-         , 	Username
-         , 	Password)
+def static "com.question.VerifyAllElements.scrollAndVerfiyElement"(
+    	String tagName	) {
+    (new com.question.VerifyAllElements()).scrollAndVerfiyElement(
+        	tagName)
 }
 
 
-def static "com.helper.login.LoginHelper.loginIntoApplicationWith"() {
-    (new com.helper.login.LoginHelper()).loginIntoApplicationWith()
-}
-
-
-def static "com.helper.login.LoginHelper.logoutFromApplication"() {
-    (new com.helper.login.LoginHelper()).logoutFromApplication()
-}
-
-
-def static "com.question.CustomDatabase.executeQuery"(
-    	String connectionUrl	
-     , 	String query	) {
-    (new com.question.CustomDatabase()).executeQuery(
-        	connectionUrl
-         , 	query)
-}
-
-
-def static "com.question.CustomSendKeys.TypeInText"(
-    	TestObject aLocator	
-     , 	String text	) {
-    (new com.question.CustomSendKeys()).TypeInText(
-        	aLocator
-         , 	text)
-}
-
-
-def static "com.question.CustomSendKeys.TypeInTextBoxWithWait"(
-    	TestObject aLocator	
-     , 	String text	
-     , 	int timeOutInSeconds	) {
-    (new com.question.CustomSendKeys()).TypeInTextBoxWithWait(
-        	aLocator
-         , 	text
-         , 	timeOutInSeconds)
+def static "com.question.BrowserTab.openNewTab"() {
+    (new com.question.BrowserTab()).openNewTab()
 }
 
 
@@ -220,37 +260,4 @@ def static "com.helper.browserhelper.CustomBrowser.openBrowserWithUrl"(
 
 def static "com.helper.browserhelper.CustomBrowser.closeBrowser"() {
     (new com.helper.browserhelper.CustomBrowser()).closeBrowser()
-}
-
-
-def static "com.question.VerifyAllElements.scrollAndVerfiyElement"(
-    	String tagName	) {
-    (new com.question.VerifyAllElements()).scrollAndVerfiyElement(
-        	tagName)
-}
-
-
-def static "com.question.VerifyMultipleElement.verifyElement"(
-    	String[] testObject	) {
-    (new com.question.VerifyMultipleElement()).verifyElement(
-        	testObject)
-}
-
-
-def static "com.question.mongo.connectRobo3t.connection"(
-    	String url	) {
-    (new com.question.mongo.connectRobo3t()).connection(
-        	url)
-}
-
-
-def static "com.question.ClickAnyLink.wrapperClick"(
-    	String linkName	) {
-    (new com.question.ClickAnyLink()).wrapperClick(
-        	linkName)
-}
-
-
-def static "com.question.BrowserTab.openNewTab"() {
-    (new com.question.BrowserTab()).openNewTab()
 }
